f40eb7d706cd3a9f9a1ba3eb216f3f8ebf0f0448
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-04-03 23:55:41 +0530

    Add files via upload

---
+++ docstring in python.py
@@ -0,0 +1,23 @@
+#It is string literal that appera right after the defination  of function 
+#class moduel
+
+def add(n1,n2): 
+    """this is the my function """
+    return n1+n2
+print(add.__doc__)
+
+# Docstring and purpose 
+# function : arguments and return values 
+# class attruibutes and methos 
+# modules : list of class function
+def add(n1,n2): 
+    """ adds two numbers and returns the sum of two numers 
+    n1=decimal integer
+    n2=decimal interger
+    return = decimal addition 
+    
+    """
+print(add.__dec___)
+help(add)
+help(print)
+
